e6abdba9269f76196777236ee2f74cd5572954f6
Amalpp <76258025+Amalpp@users.noreply.github.com>
2020-12-31 21:52:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
 $(document).ready(function(){
-		$("#inputName").blur(function(){
-			var text=$(this).val();
-			if(text.length<4)
-				{
-					$("#name1").show();
-				}
-			else{
-				$("#name1").hide();
-			}
-		});
-		
-			$("#inputNumber").blur(function(){
-			var num=$(this).val();
-			if(num.length!==10)
-				{
-					$("#number1").show();
-				}
-			else{
-				$("#number1").hide();
-			}
-		});
-		
-		$("#inputmail").blur(function(){
-			var place=$(this).val();
-			if(place.length<4)
-				{
-					$("#mail4").show();
-				}
-			else{
-				$("#mail4").hide();
-			}
-		});
-			$("#Comments").blur(function(){
-			var comment=$(this).val();
-			if(comment.length<=0)
-				{
-					$("#comment1").show();
-				}
-			else{
-				$("#comment1").hide();
-			}
-		});
-	});22
+    $("#submit-form").validate({
+        rules:{
+            Name:{
+            required:true,
+            minlength:4,
+                maxlenth:12
+            },
+            email:{
+             required:true,
+             email:true
+            },
+            Mobile:{
+				required:true,
+				minlength:10
+            },
+            comment:{
+                required:true,
+                minlength:6
+            }
+        }   
+    })
+})
